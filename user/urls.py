96c6089cddf86089b1aee49f34d5703525d458c8
JungGyu00 <co399955@gmail.com>
2023-09-07 16:19:29 +0900

    add sign-in and sign-out

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import signup
+from .views import signout, signup, signin
 
 urlpatterns = [
     path('sign-up/', signup),
+    path('sign-in/', signin),
+    path('sign-out/', signout),
 ]

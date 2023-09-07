1f247eca40a3b690123aa0d77beece611d9316f8
JungGyu00 <co399955@gmail.com>
2023-09-07 15:44:03 +0900

    add create_todo

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from todo.models import Todo
 
 # Create your views here.
 
@@ -9,3 +10,17 @@ def home(request):
         return render(request, 'todo/home.html')
     else:
         return HttpResponse('invalid request method', status='406')
+
+
+def create(request):
+    if request.user.is_authenticated:
+        if request.method == "GET":
+            return render(request, "todo/create.html")
+        elif request.method == "POST":
+            Todo.objects.create(
+                title=request.POST['title'], content=request.POST['content'], user=request.user)
+            return redirect('/todo/')
+        else:
+            return HttpResponse('invalid request method', statud='405')
+    else:
+        return redirect('/user/sign-in')

af499834439f4ed77159243fdbe6597170f01bee
JungGyu00 <co399955@gmail.com>
2023-09-07 15:06:25 +0900

    add sign-up

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    if request.method == "GET":
+        return render(request, 'todo/home.html')
+    else:
+        return HttpResponse('invalid request method', status='406')
--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from user.models import User
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == "POST":
+        User.objects.create_user(
+            username=request.POST['id'], password=request.POST['pw'])
+        return redirect('/todo/')
+    elif request.method == "GET":
+        return render(request, "user/signup.html")
+    else:
+        return HttpResponse("invalid requset method", status="406")
